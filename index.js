d747c9af95e5db13f1fbbf6ebbcb8b5f48c54ba2
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-08-01 19:32:05 +0530

    setting up modules again

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-const path = require('path');
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRoutes = require('./routes/authRoutes');
 const BlogRoutes = require('./routes/blogRoutes');
-require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 const app = express();
 
 //mongoose
 const URI = 'mongodb+srv://'+process.env.DB_USR+':'+process.env.DB_PASS+'@react-jwt-blog.o94vq.mongodb.net/blogsDB?retryWrites=true&w=majority'
-try {
-    mongoose.connect(URI, { useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false });
-    console.log('Connected to MongoDB');
-} catch (err) {
-    console.log(err);
-}
-
+mongoose.connect(URI, { useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false })
+    .then(()=>console.log("Connected to MongoDB"))
+    .catch(()=>console.log("Error Connecting"));
+    
 //middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://reactjwtblog.web.app/'],
     credentials: true
 }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
 
-//routes
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../client/build')))
-    app.use((req, res)=>{
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
-}
 app.use('/backend/authenticate',AuthRoutes);
 app.use('/backend', BlogRoutes);
 
