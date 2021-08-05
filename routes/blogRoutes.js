ecf7461a5674c2777e935901c8633fae9eef4d73
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-08-05 20:34:56 +0530

    .

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,12 +5,11 @@ const router = express.Router();
 
 router.get('/blogs', (req, res)=>{
     Post.find({}, (err, result)=>{
-        if(err){
-            console.log(err);
-            res.json(err)
-        }else{
-            console.log(result);
+        if(!err){
             res.json(result);
+        }else{
+            console.log(err);
+            res.json(err);
         }
     });
 });
@@ -19,12 +18,12 @@ router.get('/blogstitle/:title', (req, res)=>{
     postTitle = req.params.title;
     console.log(postTitle);
     Post.findOne({ title:postTitle}, (err, blog)=>{
-        if(blog){
-            console.log(blog);
+        if(!err){
             res.json(blog);
         }
-        if(err){
+        else{
             console.log(err);
+            res.json(err);
         }
     });
 });
@@ -33,12 +32,11 @@ router.get('/blogsid/:id', (req, res)=>{
     const postId = req.params.id;
     console.log(postId);
     Post.findOne({ _id:postId }, (err, blog)=>{
-        if(blog){
-            console.log(blog);
+        if(!err){
             res.json(blog);
         }
-        if(err){
-            console.log(err);
+        else{
+            res.json(err);
         }
     });
 });
@@ -51,43 +49,42 @@ router.post('/compose', (req, res)=>{
         body: postContent
     });
     post.save(function(err, result){
-        if(err){
-            res.status(200).json({
-                isError: true,
-                error: err
-            });
-        }else{
-            console.log(result);
-            res.status(200).json({
+        if(!err){
+            res.json({
                 isError: false,
                 result: result
             })
         }
+        else{
+            res.json({
+                isError: true,
+                error: err
+            });
+            
+        }
     });
 });
 
-router.post('/delete/:id', (req, res)=>{
+router.delete('/delete/:id', (req, res)=>{
     const postId = req.params.id
     console.log(postId);
     Post.findOneAndDelete({ _id:postId }, (err, result)=>{
-        if(err){
+        if(!err){
+            res.json(result);
+        }
+        else{
             console.log(err);
             res.json(err);
-        }
-        else if(!err){
-            console.log(result);
-            res.status(200).json(result);
         }
     });
 });
 
-router.post('/update/:id', (req, res)=>{
+router.put('/update/:id', (req, res)=>{
     const postId = req.params.id;
     const newPostTitle = req.body.title;
     const newPostContent = req.body.content;
     Post.findOneAndUpdate({ _id:postId }, { title: newPostTitle, body: newPostContent }, { new: true }, (err, result)=>{
         if(!err){
-            console.log(result);
             res.status(200).json({
                 isError: false,
                 result: result
